e910f3c14b885b91f7e918417f41099e85328285
Tyler Martin <sirtyler132@gmail.com>
2023-11-08 22:58:36 -0500

    Add missing passive conditions

--- src/database/passives/_IPassive.ts
+++ src/database/passives/_IPassive.ts
@@ -11,9 +11,11 @@ export enum Type {
     SUB = require("../../assets/icons/passive/slot_sub.png"),
     BATTLE_START = require("../../assets/icons/passive/slot_battle_start.png"),
     ALL = require("../../assets/icons/passive/active_all.png"),
-    DEATHMATCH = require("../../assets/icons/passive/active_deathmatch.png"),
     PVP = require("../../assets/icons/passive/active_pvp.png"),
+    DEATHMATCH = require("../../assets/icons/passive/active_deathmatch.png"),
+    DEMONICBEAST = require("../../assets/icons/passive/active_demonic_beast.png"),
     EXCEPT_PVP = require("../../assets/icons/passive/active_except_pvp.png"),
+    EXCEPT_DEATHMATCH = require("../../assets/icons/passive/active_except_deathmatch.png"),
     EXCEPT_DEMONICBEAST = require("../../assets/icons/passive/active_except_demonic_beast.png")
 }
 
